3b5ebd66d14edb0a2cddc34ac44cbb11263a5f20
wei cheng <sandsundertail99@gmail.com>
2026-02-13 13:56:43 -0500

    d
    d

--- app/src/views/Banner.vue
+++ app/src/views/Banner.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="container">
-    <h1>HSR Banner</h1>
-
     <div class="stats">
-      <p>Pulls: {{ pulls }}</p>
-      <p>5★ pity: {{ pity5 }}</p>
-      <p>4★ pity: {{ pity4 }}</p>
-      <p>Last 5★ was featured: {{ lastFeatured ? "Yes" : "No" }}</p>
+      <p class="pulls">Pulls: {{ pulls }}</p>
+      <p class="pity5">5★ pity: {{ pity5 }}</p>
+      <p class="pity4">4★ pity: {{ pity4 }}</p>
+      <p class="lastFeatured">Is the next 5★ Guaranteed: {{ lastfeatured ? 'No' : 'Yes' }}</p>
     </div>
-
-    <img @click="singlepull" src="/download (1).png" alt="" />
-    <img @click="tenpull" src="/2.png" alt="" />
     <button @click="reset">Reset</button>
 
     <div class="results">
@@ -22,17 +17,54 @@
       </ul>
     </div>
   </div>
+  <img class="single-pull-button" @click="singlepull" src="/download (1).png" alt="" />
+  <img class="ten-pull-button" @click="tenpull" src="/2.png" alt="" />
+  <img class="Lingsha-banner" src="/Let_Scent_Sink_In_2024-10-02.webp" alt="Lingsha Banner" />
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { ref } from 'vue'
 
+const featured5star = { name: 'Lingsha' }
+const standard5star = [
+  { name: 'Bronya' },
+  { name: 'Welt' },
+  { name: 'Himeko' },
+  { name: 'Seele' },
+  { name: 'Fu Xuan' },
+  { name: 'Blade' },
+  { name: 'Yanqing' },
+  { name: 'Bailu' },
+  { name: 'Gepard' },
+  { name: 'Clara' },
+]
 
-const featured5star = "Lingsha"
-const standard5star = ["Bronya", "Welt", "Himeko", "Seele", "Fu Xuan", "Blade", "Yanqing", "Bailu", "Gepard", "Clara"]
-
-const featured4star = ["Natasha", "Misha", "Guinaifen"]
-const standard4star = ["Arlan", "Asta", "Dan Heng", "Sampo", "Herta", "Gallagher", "Hanya", "Hook", "Luka", "Lynx", "March 7th", "Moze", "Pela", "Qingque", "Serval", "Susang", "Yukong", "Tingyun","Xueyi"]
+const featured4star = [
+  { name: 'Natasha', },
+  { name: 'Misha', },
+  { name: 'Guinaifen', },
+]
+const standard4star = [
+  { name: 'Arlan', },
+  { name: 'Asta', },
+  { name: 'Dan Heng', },
+  { name: 'Sampo', },
+  { name: 'Herta', },
+  { name: 'Gallagher', },
+  { name: 'Hanya', },
+  { name: 'Hook', },
+  { name: 'Luka', },
+  { name: 'Lynx', },
+  { name: 'March 7th', },
+  { name: 'Moze', },
+  { name: 'Pela', },
+  { name: 'Qingque', },
+  { name: 'Serval', },
+  { name: 'Susang', },
+  { name: 'Yukong', },
+  { name: 'Tingyun', },
+  { name: 'Xueyi', },
+]
 
 const pulls = ref(0)
 const pity5 = ref(0)
@@ -41,19 +73,21 @@ const lastfeatured = ref(true)
 const results = ref([])
 
 function roll5Star() {
-  const isfeatured = lastfeatured.value ? true : Math.random() < 0.5
+  const isfeatured = lastfeatured.value === false ? true : Math.random() < 0.5
   lastfeatured.value = isfeatured
   return {
-    name: isfeatured ? featured5star : standard5star[Math.floor(Math.random() * standard5star.length)],
-    rarity: "5★"
+    name: isfeatured
+      ? featured5star.name
+      : standard5star[Math.floor(Math.random() * standard5star.length)].name,
+    rarity: '5★',
   }
 }
 
 function roll4Star() {
-  const pool = Math.random() < 0.5 ? featured4star : standard4star
+  const pool = Math.random() < 0.5625 ? featured4star : standard4star
   return {
-    name: pool[Math.floor(Math.random() * pool.length)],
-    rarity: "4★"
+    name: pool[Math.floor(Math.random() * pool.length)].name,
+    rarity: '4★',
   }
 }
 
@@ -62,19 +96,16 @@ function gacha() {
   pity5.value++
   pity4.value++
 
-
   if (pity5.value >= 90) {
     pity5.value = 0
     pity4.value = 0
     return roll5Star()
   }
 
-
   if (pity4.value >= 10) {
     pity4.value = 0
     return roll4Star()
   }
-
 
   const r = Math.random()
 
@@ -89,7 +120,7 @@ function gacha() {
     return roll4Star()
   }
 
-  return { name: "Random Light Cone", rarity: "3★" }
+  return { name: 'Random Light Cone', rarity: '3★' }
 }
 
 function singlepull() {
@@ -129,5 +160,28 @@ function reset() {
 
 .rarity-3 {
   color: gray;
+}
+.single-pull-button {
+  position: absolute;
+  bottom: 25px;
+  right: 350px;
+  max-width: 300px;
+}
+.ten-pull-button {
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  max-width: 300px;
+}
+.container {
+  position: absolute;
+  top: 0px;
+  left: 100px;
+}
+.Lingsha-banner {
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  width: 1400px;
 }
 </style>
